1f98fc62a90e2d8138dadbb9ac211cc27b222df0
juanduquevan <juanduquevan@gmail.com>
2023-06-08 18:12:27 -0400

    Script changes.

--- run_vip.sh
+++ run_vip.sh
@@ -7,7 +7,7 @@ for lr in ${LRS[@]}; do
     for seed in ${SEEDS[@]}; do
         for kl in ${KLS[@]}; do
             export WANDB_NAME=vip_seed_${seed}_lr_${lr}_kl_${kl}
-            sbatch --job-name=cg_eg_vip run_vip.slurm ${seed} ${lr} ${kl}
+            sbatch --job-name=cg_eg_vip run_vip.slurm ${seed} ${lr} ${kl} --exclude=cn-g023
         done
     done
 done

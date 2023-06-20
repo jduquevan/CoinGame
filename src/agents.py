KL bug fix.

src/agents.py
@@ -245,7 +245,7 @@ class VIPAgent(BaseAgent):
         no_info = -1*torch.ones(self.representation_size).to(self.device)
         _, pi_info = self.actor(torch.cat([state_a, agent_r.flatten()]), h_a)
         _, pi_no_info = self.actor(torch.cat([state_a, no_info]), h_a)
-        kl = torch.sum(pi_no_info * torch.log(torch.div(pi_no_info, pi_info)))
+        kl = torch.sum(pi_no_info * torch.log(torch.div(pi_no_info, pi_info.detach())))
         return kl
     
     def compute_kl_divergences(self, states_a, agent_r, h_a):
@@ -254,7 +254,7 @@ class VIPAgent(BaseAgent):
         _, pi_no_info = self.actor.batch_forward(torch.cat([states_a, no_info], dim=1), h_a)
         pi_info = pi_info.reshape(self.batch_size, -1)
         pi_no_info = pi_no_info.reshape(self.batch_size, -1)
-        kl = torch.mean(torch.sum(pi_no_info * torch.log(torch.div(pi_no_info, pi_info)), dim=1))
+        kl = torch.mean(torch.sum(pi_no_info * torch.log(torch.div(pi_no_info, pi_info.detach())), dim=1))
         return kl
 
     def compute_entropy_normalized(self, state_a, agent_r, h_a):
09ba86192d46fea296ee3ff3abb6a3f52c6e965e
Juan Duque <juan.duque@mila.quebec>
2023-06-24 13:10:36 -0400

    Narval script file modification

--- run_vip_narval.sh
+++ run_vip_narval.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 SEEDS=(1)
 LRS=(0.005)
-GPS=(0.1 0.9)
+GPS=(0.7)
 ENTS=(0.01)
-KLS=(0.001 0.005 0.01 0.05 0.1)
+KLS=(0.005)
+HLENS=(8)
 
 for lr in ${LRS[@]}; do
     for seed in ${SEEDS[@]}; do
         for gp in ${GPS[@]}; do
             for ent in ${ENTS[@]}; do
                 for kl in ${KLS[@]}; do
-                    export WANDB_NAME=vip_seed_${seed}_lr_${lr}_gp_${gp}_ent_${ent}_kl_${kl}
-                    sbatch --job-name=cg_eg_vip run_vip_narval.slurm ${seed} ${lr} ${gp} ${ent} ${kl}
+                    for hlen in ${HLENS[@]}; do
+                        export WANDB_NAME=vip_seed_${seed}_lr_${lr}_gp_${gp}_ent_${ent}_kl_${kl}_hlen_${hlen}
+                        sbatch --job-name=cg_eg_vip run_vip_narval.slurm ${seed} ${lr} ${gp} ${ent} ${kl} ${hlen}
+                    done
                 done
             done
         done

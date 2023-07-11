8d11bb9718ab29a1c5b43deab450b44941cd7fa2
Juan Duque <juan.duque@mila.quebec>
2023-07-10 22:49:00 -0400

    Narval script changes.

--- run_vip_narval.sh
+++ run_vip_narval.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 SEEDS=(7)
 LRS=(0.0005)
-GPS=(0.6 0.65 0.7)
-ENTS=(0.015 0.03)
+ILRS=(0.005 0.006 0.007)
+GPS=(0)
+ENTS=(0.03 0.05)
 KLS=(0.05)
 HLENS=(8)
-RDPS=(0.8 0.75 0.7)
+RDPS=(1.0)
+
 
 for lr in ${LRS[@]}; do
     for seed in ${SEEDS[@]}; do
@@ -14,8 +16,10 @@ for lr in ${LRS[@]}; do
                 for kl in ${KLS[@]}; do
                     for hlen in ${HLENS[@]}; do
                         for rdp in ${RDPS[@]}; do
-                            export WANDB_NAME=vip_seed_${seed}_lr_${lr}_gp_${gp}_ent_${ent}_kl_${kl}_hlen_${hlen}_rdp_${rdp}
-                            sbatch --job-name=cg_eg_vip run_vip_narval.slurm ${seed} ${lr} ${gp} ${ent} ${kl} ${hlen} ${rdp}
+                            for ilr in ${ILRS[@]}; do
+                                export WANDB_NAME=vip_seed_${seed}_lr_${lr}_gp_${gp}_ent_${ent}_kl_${kl}_hlen_${hlen}_rdp_${rdp}_irl_${ilr}
+                                sbatch --job-name=cg_eg_vip run_vip_narval.slurm ${seed} ${lr} ${gp} ${ent} ${kl} ${hlen} ${rdp} ${ilr}
+                            done 
                         done
                     done
                 done

efab5e404ad0ac63bc7cb9741f95817d7463ca43
Juan Duque <juan.duque@mila.quebec>
2023-08-07 19:31:00 -0400

    Narval script changes.

--- run_vip_narval_v2.sh
+++ run_vip_narval_v2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SEEDS=(8)
-ALRS=(0.000001 0.00001 0.0001)
-CLRS=(0.0005 0.001 0.005)
-OPTS=("eg" "adam" "om")
-INFS=(1)
-ENTS=(0.1)
+ALRS=(0.00001)
+CLRS=(0.001)
+OPTS=("eg")
+INFS=(0.9)
+ENTS=(0.1 0.2 0.3)
 
 for alr in ${ALRS[@]}; do
     for clr in ${CLRS[@]}; do
@@ -12,7 +12,7 @@ for alr in ${ALRS[@]}; do
             for inf in ${INFS[@]}; do
                 for ent in ${ENTS[@]}; do
                     for seed in ${SEEDS[@]}; do
-                        export WANDB_NAME=vip_v2_seed_${seed}_alr_${alr}_clr_${clr}_opt_${opt}_inf_${inf}_ent_${ent}
+                        export WANDB_NAME=vip_v2_mask_not_-A_R_seed_${seed}_alr_${alr}_clr_${clr}_opt_${opt}_inf_${inf}_ent_${ent}
                         sbatch --job-name=ipd_vip run_vip_narval_v2.slurm ${seed} ${alr} ${clr} ${opt} ${inf} ${ent}
                     done           
                 done
